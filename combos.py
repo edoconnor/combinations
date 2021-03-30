5c09c8c4ab297de699e14f8c0072e5206fd30bca
Ed O'Connor <ed@oconnor.app>
2021-03-30 13:23:42 -0400

    added 6th column plus comments

--- combos.py
+++ combos.py
@@ -2,22 +2,25 @@ import pandas as pd
 from itertools import combinations 
 import time 
 
+## import players and salaries:
 df = pd.read_csv("data.csv") 
 df = df[['player', 'salary']]
 
 player = df['player']
-combos = list(combinations(player,5))
+combos = list(combinations(player,6))
 
-df5 = pd.DataFrame(combos, columns =['A', 'B', 'C', 'D', 'E']) 
+df5 = pd.DataFrame(combos, columns =['A', 'B', 'C', 'D', 'E', 'F']) 
 
+## calculate total salary for each combination:
 df5['AA'] = df5.A.replace(df.set_index('player').salary.to_dict())
 df5['BB'] = df5.B.replace(df.set_index('player').salary.to_dict())
 df5['CC'] = df5.C.replace(df.set_index('player').salary.to_dict())
 df5['DD'] = df5.D.replace(df.set_index('player').salary.to_dict())
 df5['EE'] = df5.E.replace(df.set_index('player').salary.to_dict())
-df5['F'] = (df5['AA'] + df5['BB'] + df5['CC'] + df5['DD'] + df5['EE'])
+df5['FF'] = df5.F.replace(df.set_index('player').salary.to_dict())
+df5['G'] = (df5['AA'] + df5['BB'] + df5['CC'] + df5['DD'] + df5['EE'] + df5['FF'])
 
-df6 = df5[['A', 'B', 'C', 'D', 'E', 'F']]
+df6 = df5[['A', 'B', 'C', 'D', 'E', 'F', 'G']]
 
 print(df6)
 df = df6
